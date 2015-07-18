27c3f84e1a473a158e413a932f4e4c7d3cf00627
Stephan Balmer <sb@cis.ch>
2015-07-18 23:17:02 +0200

    issue

--- controllers/Stanton-SCS3d-scripts.js
+++ controllers/Stanton-SCS3d-scripts.js
@@ -2,6 +2,7 @@
 // - Hold EQ-PITCH to reset pitch/rate
 // - Each deck rembembers the mode it was in, confusing? Would it be better to
 //   keep the current mode on deck switch?
+// - Current FX chain does not switch with deck, but mode does. This is confusing.
 
 // Useful tinkering commands, channel reset and flat mode
 // amidi -p hw:1 -S F00001600200F7
